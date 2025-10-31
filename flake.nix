7ba773d7f9375b8aed46dbffdf3ee8c4e235f97d
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2025-10-31 22:31:59 +0100

    feat: Add `package.default` to flake

--- flake.nix
+++ flake.nix
@@ -1,21 +1,30 @@
 {
-  description = "Description for the project";
+  description = "A tool to update my school logbook directly from the terminal";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     devshell.url = "github:numtide/devshell";
+    crane.url = "github:ipetkov/crane";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs = inputs@{ flake-parts, crane, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         inputs.devshell.flakeModule
       ];
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      flake = {
-      };
-      perSystem = { config, pkgs, ...}: {
+      perSystem = { pkgs, ...}:
+        let
+            craneLib = crane.mkLib pkgs;
+        in {
+        packages.default = craneLib.buildPackage {
+          src = craneLib.cleanCargoSource ./.;
+          buildInputs = with pkgs; [
+            openssl
+            pkg-config
+          ];
+        };
         devshells.default = {
           env = [
             {
@@ -31,9 +40,9 @@
               value = 1;
             }
           ];
-          packages = [
-            pkgs.openssl
-            pkgs.pkg-config
+          packages = with pkgs; [
+            openssl
+            pkg-config
           ];
         };
       };
